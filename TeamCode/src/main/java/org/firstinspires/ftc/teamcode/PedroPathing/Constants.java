50ebd7250c00c2c878fe49fc04cd848ae385805d
User234233 <smrithi9.srivats@gmail.com>
2026-08-27 18:16:07 -0700

    starting pedro tuning

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PedroPathing/Constants.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.PedroPathing;
+
+import com.pedropathing.control.PIDFCoefficients;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.CoaxialPod;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.drivetrains.SwerveConstants;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@Autonomous
+public class Constants {
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(35);
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("frontRight")
+            .rightRearMotorName("backRight")
+            .leftRearMotorName("frontLeft")
+            .leftFrontMotorName("backLeft")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+
+    public static SwerveConstants swerveConstants = new SwerveConstants()
+            .maxPower(1); //determines the max power of the drivetrain
+    //      .zeroPowerBehavior(SwerveConstants.ZeroPowerBehavior.IGNORE_ANGLE_CHANGES)
+    //the above disables x locking for swerve, which can be useful for tuning pod offsets
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
+                .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
+                .build();
+    }
+}
+
